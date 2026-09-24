b9234729eebeff64c197cb7167c3eb50a6b08db7
gabrielleendres-dev <gabbykendres@gmail.com>
2026-09-24 16:40:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def check_password():
     if st.session_state.get("authenticated", False):
         return True
 
-   st.sidebar.title("💪 My Executive Assistant")
+   st.sidebar.title("💪 My Assistant")
 
 if st.sidebar.button("Lock Dashboard"):
     st.session_state.authenticated = False
