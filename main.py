0fa72f35a6e588c7df4ff7fc431992ada16b33ab
gabrielleendres-dev <gabbykendres@gmail.com>
2026-09-24 17:29:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -407,4 +407,5 @@ page = st.sidebar.radio(
 # =========================================================
 
 if page == "Dashboard":
-    st.title("Personal + Executive Assistant
+    st.title("Personal + Executive Assistant)
+            
